b3cc868fc9c0cc802f718099dfefd4e3295b5839
sebax774 <bugansebastian@gmail.com>
2020-11-13 07:33:13 +0100

    Add project files.

---
+++ Open-Lab-10.05/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Open_Lab_10._05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
